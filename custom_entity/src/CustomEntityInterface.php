9d6ae05c5d85d3dd77252d7fea6ab497b0586900
abhijiths-srijan <abhijith.s@srijan.net>
2022-09-21 19:39:30 +0530

    Entity Creation sample

---
+++ custom_entity/src/CustomEntityInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\custom_entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface defining a custom entity entity type.
+ */
+interface CustomEntityInterface extends ContentEntityInterface, EntityOwnerInterface, EntityChangedInterface {
+
+}
---
+++ custom_entity/src/Entity/CustomEntity.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Drupal\custom_entity\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\custom_entity\CustomEntityInterface;
+use Drupal\user\EntityOwnerTrait;
+
+/**
+ * Defines the custom entity entity class.
+ *
+ * @ContentEntityType(
+ *   id = "custom_entity",
+ *   label = @Translation("Custom Entity"),
+ *   label_collection = @Translation("Custom Entities"),
+ *   label_singular = @Translation("custom entity"),
+ *   label_plural = @Translation("custom entities"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count custom entities",
+ *     plural = "@count custom entities",
+ *   ),
+ *   handlers = {
+ *     "list_builder" = "Drupal\custom_entity\CustomEntityListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "access" = "Drupal\custom_entity\CustomEntityAccessControlHandler",
+ *     "form" = {
+ *       "add" = "Drupal\custom_entity\Form\CustomEntityForm",
+ *       "edit" = "Drupal\custom_entity\Form\CustomEntityForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     }
+ *   },
+ *   base_table = "custom_entity",
+ *   admin_permission = "administer custom entity",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid",
+ *     "owner" = "uid",
+ *   },
+ *   links = {
+ *     "collection" = "/admin/content/custom-entity",
+ *     "add-form" = "/custom-entity/add",
+ *     "canonical" = "/custom-entity/{custom_entity}",
+ *     "edit-form" = "/custom-entity/{custom_entity}/edit",
+ *     "delete-form" = "/custom-entity/{custom_entity}/delete",
+ *   },
+ *   field_ui_base_route = "entity.custom_entity.settings",
+ * )
+ */
+class CustomEntity extends ContentEntityBase implements CustomEntityInterface {
+
+  use EntityChangedTrait;
+  use EntityOwnerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(EntityStorageInterface $storage) {
+    parent::preSave($storage);
+    if (!$this->getOwnerId()) {
+      // If no owner has been set explicitly, make the anonymous user the owner.
+      $this->setOwnerId(0);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['label'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Label'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Status'))
+      ->setDefaultValue(TRUE)
+      ->setSetting('on_label', 'Enabled')
+      ->setDisplayOptions('form', [
+        'type' => 'boolean_checkbox',
+        'settings' => [
+          'display_label' => FALSE,
+        ],
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'boolean',
+        'label' => 'above',
+        'weight' => 0,
+        'settings' => [
+          'format' => 'enabled-disabled',
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['description'] = BaseFieldDefinition::create('text_long')
+      ->setLabel(t('Description'))
+      ->setDisplayOptions('form', [
+        'type' => 'text_textarea',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'text_default',
+        'label' => 'above',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Author'))
+      ->setSetting('target_type', 'user')
+      ->setDefaultValueCallback(static::class . '::getDefaultEntityOwner')
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => 60,
+          'placeholder' => '',
+        ],
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'author',
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setDescription(t('The time that the custom entity was created.'))
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the custom entity was last edited.'));
+
+    return $fields;
+  }
+
+}
